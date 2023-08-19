7db3dfcc306c24484ef757e25ed5395ecdb0e4b7
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2023-08-19 17:55:46 +1000

    added stub functions for 12 additional sorts, updated command loop  calls and help function implementation to reflect the changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import genDataSets
+import sorts
 
 def commandLoop():
     print(formatPrompt("input: "), end="")
@@ -8,7 +9,44 @@ def commandLoop():
             case "h":
                 help_pySort()
             case "sel":
-                print(selectionSort(genDataSets.genRandArr(int(input()))))
+                print("n: ", end='')
+                print(sorts.selectionSort(genDataSets.genRandArr(int(input()))))
+            case "bub":
+                print("n: ", end='')
+                print(sorts.bubbleSort(genDataSets.genRandArr(int(input()))))
+            case "ins":
+                print("n: ", end='')
+                print(sorts.insertionSort(genDataSets.genRandArr(int(input()))))
+            case "hep":
+                print("n: ", end='')
+                print(sorts.heapSort(genDataSets.genRandArr(int(input()))))
+            case "qck":
+                print("n: ", end='')
+                print(sorts.quickSort(genDataSets.genRandArr(int(input()))))
+            case "mrg":
+                print("n: ", end='')
+                print(sorts.mergeSort(genDataSets.genRandArr(int(input()))))
+            case "bck":
+                print("n: ", end='')
+                print(sorts.bucketSort(genDataSets.genRandArr(int(input()))))
+            case "rdx":
+                print("n: ", end='')
+                print(sorts.radixSort(genDataSets.genRandArr(int(input()))))
+            case "cnt":
+                print("n: ", end='')
+                print(sorts.countSort(genDataSets.genRandArr(int(input()))))
+            case "shl":
+                print("n: ", end='')
+                print(sorts.shellSort(genDataSets.genRandArr(int(input()))))
+            case "tim":
+                print("n: ", end='')
+                print(sorts.timSort(genDataSets.genRandArr(int(input()))))
+            case "tre":
+                print("n: ", end='')
+                print(sorts.treeSort(genDataSets.genRandArr(int(input()))))
+            case "cbe":
+                print("n: ", end='')
+                print(sorts.cubeSort(genDataSets.genRandArr(int(input()))))
             case _:
                 print("command not recognised")
         print(formatPrompt("input: "), end="")
@@ -19,20 +57,21 @@ def help_pySort():
     print("  - h: print this message")
     print("  - q or Q: quit the program")
     print("  - sel: selection sort")
+    print("  - ins: selection sort")
+    print("  - hep: selection sort")
+    print("  - qck: selection sort")
+    print("  - mrg: selection sort")
+    print("  - bck: bucket sort")
+    print("  - rdx: selection sort")
+    print("  - cnt: selection sort")
+    print("  - shl: selection sort")
+    print("  - tim: selection sort")
+    print("  - tre: selection sort")
+    print("  - cbe: selection sort")
 
 def formatPrompt(promptMsg):
     return ("\033[94m {}\033[00m".format(promptMsg)).strip()
     
-def selectionSort(arr):
-    minIdx, currIdx = 0, 0
-    for i in range(len(arr) - 1):
-        for j in range(currIdx, len(arr)):
-            if (arr[j] < arr[minIdx]):
-                minIdx = j
-        arr[minIdx], arr[currIdx] = arr[currIdx], arr[minIdx]
-        minIdx, currIdx = currIdx + 1, currIdx + 1
-    return arr
-        
 if __name__ == '__main__': #
     print(formatPrompt("Welcome to pySort, please enter a command:"))
     commandLoop()
---
+++ sorts.py
@@ -0,0 +1,57 @@
+def selectionSort(arr):
+    minIdx, currIdx = 0, 0
+    for i in range(len(arr) - 1):
+        for j in range(currIdx, len(arr)):
+            if (arr[j] < arr[minIdx]):
+                minIdx = j
+        arr[minIdx], arr[currIdx] = arr[currIdx], arr[minIdx]
+        minIdx, currIdx = currIdx + 1, currIdx + 1
+    return arr
+        
+def bubbleSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def insertionSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def heapSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def quickSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def mergeSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def bucketSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def radixSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def countSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def shellSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def timSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def treeSort(arr):
+    print("TODO - implement this sort")
+    return arr
+
+def cubeSort(arr):
+    print("TODO - implement this sort")
+    return arr
