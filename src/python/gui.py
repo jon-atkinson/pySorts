e1a09fd58180cb3f1410089c143ba50f16064612
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2023-12-06 18:56:55 +1100

    exploring tkinter functionality

--- src/python/gui.py
+++ src/python/gui.py
@@ -2,10 +2,18 @@ from tkinter import *
 
 root = Tk()
 
-# Creating a Label Widget
-myLabel = Label(root, text="Hello World!")
+def myClick(count):
+    myLabel = Label(root, text="Look! I clicked a button!!").grid(row=count,column=0)
 
-# Shoving it onto the screen
-myLabel.pack()
+count = 2
+
+# Creating and gridding labels
+myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text="My Name in Jon Atkinson").grid(row=1, column=1)
+
+myButton = Button(root, text="Click Me!", state=DISABLED).grid(row=0,column=1)
+myButton = Button(root, text="No, Click Me!", padx=50, pady=5, command=myClick(count)).grid(row=1,column=0)
+
+# counter = Button(root, text=)
 
 root.mainloop()
