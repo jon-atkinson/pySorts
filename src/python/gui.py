3ff4aa6bf842d23210a2763b4e300acaf002c7d5
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2023-12-15 04:53:52 +1100

    adding resizeable kernel support

--- src/python/gui.py
+++ src/python/gui.py
@@ -7,6 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from plot import plot_algos_gui
 from statistics import median
 import numpy as np
+import math
 
 # colour pallet
 colour1 = "#B8D8D8"
@@ -578,18 +579,19 @@ class GraphPage(tk.Frame):
                             pady=5)
         self.rightPanel = ttk.Frame(self)
         self.rightPanel.grid(row=0,
-                            column=1,
-                            padx=(0, 5))
+                             column=1,
+                             padx=(0, 5))
 
         homeButton = ttk.Button(self.rightPanel,
-                             text="Back to Home",
-                             style="Home.TButton",
-                             command=lambda: controller.show_frame(StartPage))
+                                text="Back to Home",
+                                style="Home.TButton",
+                                command=lambda: controller.show_frame(StartPage))
         homeButton.grid(row=0,
                         column=0,
                         padx=10,
                         pady=10,
                         sticky="nsew")
+
         resetFiltersButton = ttk.Button(self.rightPanel,
                                         text="Reset Filters",
                                         style="Home.TButton",
@@ -599,6 +601,7 @@ class GraphPage(tk.Frame):
                                 padx=10,
                                 pady=10,
                                 sticky="nsew")
+
         removePeaksButton = ttk.Button(self.rightPanel,
                                       text="Remove Peaks\n(median filter)",
                                       command=self.medianFilterPlots)
@@ -607,19 +610,68 @@ class GraphPage(tk.Frame):
                               padx=10,
                               pady=10,
                               sticky="nsew")
+
+        slideContain = ttk.Frame(self.rightPanel)
+        slideContain.grid(row=3,
+                          column=0)
+        self.medianFilterSize = tk.IntVar(value=3)
+        self.medianFilterSizeSlider = ttk.Scale(slideContain,
+                                               from_=3,
+                                               to=25,
+                                               orient="horizontal",
+                                               length=100,
+                                               command=self.onMedianKernelSizeSliderChange,
+                                               variable=self.medianFilterSize)
+        self.medianFilterSizeSlider.grid(row=0,
+                                        column=0)
+        self.medianFilterSizeLabel = ttk.Label(slideContain,
+                                          text="M Size: 03")
+        self.medianFilterSizeLabel.grid(row=0,
+                                       column=1)
+
         smoothPlotsButton = ttk.Button(self.rightPanel,
-                                      text="Smooth Plots\n(convolution filter)",
+                                      text="Smooth Plots\n(conv. filter)",
                                       command=self.convolveFilterPlots)
-        smoothPlotsButton.grid(row=3,
-                              column=0,
-                              padx=10,
-                              pady=10,
-                              sticky="nsew")
+        smoothPlotsButton.grid(row=4,
+                               column=0,
+                               padx=10,
+                               pady=10,
+                               sticky="nsew")
+
+        slideContain = ttk.Frame(self.rightPanel)
+        slideContain.grid(row=5,
+                          column=0)
+        self.gaussKernelSize = tk.IntVar(value=3)
+        self.gaussKernelSizeSlider = ttk.Scale(slideContain,
+                                               from_=3,
+                                               to=25,
+                                               orient="horizontal",
+                                               length=100,
+                                               command=self.onGaussKernelSizeSliderChange,
+                                               variable=self.gaussKernelSize)
+        self.gaussKernelSizeSlider.grid(row=0,
+                                        column=0)
+        self.gaussKernelSizeLabel = ttk.Label(slideContain,
+                                              text="K Size: 03")
+        self.gaussKernelSizeLabel.grid(row=0,
+                                       column=1)
 
         self.leftPanel.f = Figure(dpi=100)
         self.leftPanel.f.set_size_inches(11, 6.4, forward=False)
         self.canvas = FigureCanvasTkAgg(self.leftPanel.f, self.leftPanel)
         self.canvas.draw()
+    
+    def onGaussKernelSizeSliderChange(self, value):
+        intVal = max(1, round(float(self.gaussKernelSize.get())))
+        oddVal = intVal + (intVal % 2 == 0)
+        self.gaussKernelSize.set(oddVal)
+        self.gaussKernelSizeLabel.config(text=f"K Size: {round(float(self.gaussKernelSize.get())):02}")
+
+    def onMedianKernelSizeSliderChange(self, value):
+        intVal = max(1, round(float(self.medianFilterSize.get())))
+        oddVal = intVal + (intVal % 2 == 0)
+        self.medianFilterSize.set(oddVal)
+        self.medianFilterSizeLabel.config(text=f"M Size: {round(float(self.medianFilterSize.get())):02}")
 
     def plot(self, results, n_steps, alg_names, num_reps):
         self.leftPanel.f.clear()
@@ -648,23 +700,31 @@ class GraphPage(tk.Frame):
         self.plot(self.originalResults, self.n_steps, self.alg_names, self.num_reps)
 
     def medianFilterPlots(self):
+        halfLen = self.medianFilterSize.get() // 2
         smoothedResults = {}
         for algo in self.results:
             smoothed = []
-            paddedResults = [0,0] + self.results[algo] + [10000,10000]
-            for r in range(2, len(paddedResults) - 2):
-                smoothed.append(median(paddedResults[r-2:r+3]))
+            paddedResults = self.results[algo][:halfLen][::-1] + self.results[algo] + self.results[algo][-halfLen:][::-1]
+            for r in range(halfLen, len(paddedResults) - halfLen):
+                smoothed.append(median(paddedResults[r-halfLen:r+halfLen+1]))
             smoothedResults[algo] = smoothed
         self.plot(smoothedResults, self.n_steps, self.alg_names, self.num_reps)
 
     def convolveFilterPlots(self):
-        kernel = np.array([1, 4, 6, 4, 1]) / 16
+        kernel = self.genKernel()
         smoothedResults = {}
+        buffLen = self.gaussKernelSize.get() // 2
 
         for algo in self.results:
-            smoothedResults[algo] = self.results[algo][:2] + list(np.convolve(self.results[algo], kernel, mode="valid")) + self.results[algo][-2:]
+            smoothedResults[algo] = self.results[algo][:buffLen] + list(np.convolve(self.results[algo], kernel, mode="valid")) + self.results[algo][-buffLen:]
 
         self.plot(smoothedResults, self.n_steps, self.alg_names, self.num_reps)
+
+    def genKernel(self):
+        length = self.gaussKernelSize.get()
+        sigma = 1
+        r = range(-int(length/2),int(length/2)+1)
+        return [1 / (sigma * math.sqrt(2*math.pi)) * math.exp(-float(x)**2/(2*sigma**2)) for x in r]
 
 
 app = app()
