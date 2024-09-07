5a84d63c56441b9e21671c9ed8857c41ad4e5e63
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2024-09-07 11:23:10 +1000

    removing build artifacts

--- build/lib/pthn/gui/colour.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-class Colour(StrEnum):
-    # colour pallet
-    colour1 = "#B8D8D8"
-    colour2 = "#7A9E9F"
-    colour3 = "#4F6367"
-    colour4 = "#EEF5DB"
-    colour5 = "#FE5F55"
--- build/lib/pthn/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='pySorts',
-      version='0.01',
-      description='simple sorts comparison',
-      author='Jon Atkinson'
-     )
