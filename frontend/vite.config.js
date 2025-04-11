04f5fcda3f598c6b9ae9abc234f38644d7bd9707
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-04-12 00:17:20 +1000

    remove commented code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,4 @@ export default defineConfig({
   server: {
     port: 5173,
   },
-  // resolve: {
-  //   alias: {
-  //     crypto: "crypto-browserify",
-  //     stream: "stream-browserify",
-  //   },
-  // },
 });
